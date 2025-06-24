f31493e244fb08b0e7fda8afa616ff0ef856ab9c
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-06-24 13:46:28 +0530

    added karma.js

---
+++ karma.conf.js
@@ -0,0 +1,42 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        // Jasmine options can go here
+      },
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/dockey'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    
+    // Use custom launcher
+    browsers: ['ChromeHeadlessNoSandbox'],
+    
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+
+    restartOnFileChange: true
+  });
+};
